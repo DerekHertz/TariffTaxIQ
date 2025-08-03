1f46b1b8911ca7ad660c92baddde193b9474d179
Derek <derekhertzell@gmail.com>
2025-08-03 13:56:15 -0700

    black

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,11 @@ app = FastAPI(title="Tariff Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://tarifftaxiq.org", "https://www.tarifftaxiq.org"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://tarifftaxiq.org",
+        "https://www.tarifftaxiq.org",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
